32b73aaead6eacd383b33c8ff9f1996fb97925b7
Mario Vellandi <mvellandi@hotmail.com>
2023-09-26 20:04:50 +0200

    ~ rename location name variable ~ change temp from low to high

--- lib/himmel_web/components/components.ex
+++ lib/himmel_web/components/components.ex
@@ -8,15 +8,15 @@ defmodule HimmelWeb.WeatherComponents do
     ~H"""
     <div id={@id} class="flex justify-between items-center rounded-xl bg-red-dark py-3.5 px-4">
       <div class="flex flex-col">
-        <h2 class="text-2xl font-bold leading-none"><%= @data.name %></h2>
+        <h2 class="text-2xl font-bold leading-none"><%= @data.place %></h2>
         <h3 class="font-semibold"><%= @data.description %></h3>
         <h4 class="font-semibold text-red-light pt-6">DELETE</h4>
       </div>
       <div class="flex flex-col h-full justify-between items-end">
         <span class="text-5xl font-light leading-[0.9]"><%= @data.temperature %>&deg;</span>
         <div class="flex justify-end gap-5 font-semibold">
-          <h4>H: <%= @data.low %>&deg;</h4>
           <h4>L: <%= @data.high %>&deg;</h4>
+          <h4>H: <%= @data.low %>&deg;</h4>
         </div>
       </div>
     </div>
