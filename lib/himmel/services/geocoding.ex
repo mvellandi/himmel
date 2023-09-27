d9a34262ffe5cfb65f8b94bb1c10c048514e4c36
Mario Vellandi <mvellandi@hotmail.com>
2023-09-27 19:56:43 +0200

    + add fields needed for client search

--- lib/himmel/services/geocoding.ex
+++ lib/himmel/services/geocoding.ex
@@ -8,5 +8,16 @@ defmodule Himmel.Services.Geocoding do
     ("https://geocoding-api.open-meteo.com/v1/search?count=20&language=en&format=json&" <>
        "name=#{name}")
     |> json_request()
+    |> Map.get("results")
+    |> Enum.map(fn result ->
+      %{
+        "name" => result["name"],
+        "region" => result["admin1"],
+        "country" => result["country"],
+        "latitude" => result["latitude"] |> to_string(),
+        "longitude" => result["longitude"] |> to_string(),
+        "provider_place_id" => result["id"] |> to_string()
+      }
+    end)
   end
 end
