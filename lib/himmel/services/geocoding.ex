7c01f20c5a9df3c4f3f05490b73c6056e279a2b4
Mario Vellandi <mvellandi@hotmail.com>
2023-09-29 00:18:44 +0200

    ~ mod key names from strings to atoms ~ mod lat/long from strings to ints ~ rename id key

--- lib/himmel/services/geocoding.ex
+++ lib/himmel/services/geocoding.ex
@@ -2,7 +2,7 @@ defmodule Himmel.Services.Geocoding do
   import Himmel.Utils
 
   @doc "Gets a list of search results for a place"
-  def find_place(place) when is_binary(place) do
+  def search_places(place) when is_binary(place) do
     name = String.split(place) |> Enum.join("+")
 
     ("https://geocoding-api.open-meteo.com/v1/search?count=20&language=en&format=json&" <>
@@ -11,12 +11,12 @@ defmodule Himmel.Services.Geocoding do
     |> Map.get("results")
     |> Enum.map(fn result ->
       %{
-        "name" => result["name"],
-        "region" => result["admin1"],
-        "country" => result["country"],
-        "latitude" => result["latitude"] |> to_string(),
-        "longitude" => result["longitude"] |> to_string(),
-        "provider_place_id" => result["id"] |> to_string()
+        name: result["name"],
+        region: result["admin1"],
+        country: result["country"],
+        latitude: result["latitude"],
+        longitude: result["longitude"],
+        id: result["id"]
       }
     end)
   end
