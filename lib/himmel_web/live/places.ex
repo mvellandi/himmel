a0c3f85d9fa4362c0d4589045af73f080ac12c6d
Mario Vellandi <mvellandi@hotmail.com>
2023-10-05 11:33:55 +0200

    ~ move search form to function component + add clear search field button and handlers ~ refactor show search results, formatting + hide places list if there's search results

--- lib/himmel_web/live/places.ex
+++ lib/himmel_web/live/places.ex
@@ -22,66 +22,64 @@ defmodule HimmelWeb.PlacesLive do
       <%!-- TODO: See if I can better handle hiding the saved places list during search results --%>
       <%!-- phx-focus={JS.hide(to: "#places-list") |> JS.show(to: "#search-results")}
           phx-blur={JS.show(to: "#places-list") |> JS.hide(to: "#search-results")} --%>
-      <search>
-        <form phx-submit="search_places" phx-target={@myself} class="inline-flex items-center justify-between w-full h-10 rounded-xl bg-red-dark text-red-light py-2 pl-2">
-          <input
-            type="text"
-            name="name"
-            value={@search}
-            placeholder="Search for a city or place"
-            autocomplete="off"
-            phx-debounce="200"
-            class="w-full bg-transparent text-white placeholder:text-red-light pl-2"
-          />
-          <button class="px-4 transition ease-in-out duration-150 outline-none">
-            <.icon_loupe />
-          </button>
-        </form>
-      </search>
+      <.search_bar search={@search} myself={@myself} />
       <%!-- SEARCH RESULT LIST --%>
-      <div id="search-results" class="">
-        <%= if not is_nil(@search_results) do %>
-        <ul class="flex flex-col gap-2">
-          <%= if @search_results == [] && @search !== "" do %>
-            <li class="flex justify-left items-center rounded-xl bg-red-dark py-3.5 px-4">
-              <h2 class="text-2xl font-bold">No results found</h2>
-            </li>
-          <% else %>
-            <%= @search_results |> Enum.with_index |> Enum.map(fn({result, index}) -> %>
-              <li id={"result-#{index}"} phx-target={@myself} phx-click="add_place" phx-value-id={result.id} >
-                <div class="border-2 border-red-dark px-4 py-2 cursor-pointer rounded-xl bg-red-dark hover:border-red-medium hover:border-2">
-                  <h2 class="text-2xl font-bold"><%= result.name %></h2>
-                  <h3 class="font-semibold"><%= result.region %>, <%= result.country %></h3>
-                </div>
+      <div id="search-results">
+        <%= if @search_results do %>
+          <ul class="flex flex-col gap-2">
+            <%= if @search_results == [] && @search !== "" do %>
+              <li class="flex justify-left items-center rounded-xl bg-red-dark py-3.5 px-4">
+                <h2 class="text-2xl font-bold">No results found</h2>
               </li>
-            <% end) %>
-          <% end %>
-        </ul>
+            <% else %>
+              <%= @search_results |> Enum.with_index |> Enum.map(fn({result, index}) -> %>
+                <li
+                  id={"result-#{index}"}
+                  phx-target={@myself}
+                  phx-click="add_place"
+                  phx-value-id={result.id}
+                >
+                  <div class="border-2 border-red-dark px-4 py-2 cursor-pointer rounded-xl bg-red-dark hover:border-red-medium hover:border-2">
+                    <h2 class="text-2xl font-bold"><%= result.name %></h2>
+                    <h3 class="font-semibold"><%= result.region %>, <%= result.country %></h3>
+                  </div>
+                </li>
+              <% end) %>
+            <% end %>
+          </ul>
         <% end %>
       </div>
       <%!-- PLACES LIST --%>
       <div id="places-list" class="flex flex-col space-y-3">
-        <%!-- MY LOCATION (CUSTOM SIZE) --%>
-        <div id="myLocation" phx-click="set_main_weather_to_my_location" class="flex justify-between items-center rounded-xl bg-red-dark py-3.5 px-4 cursor-pointer">
-          <div class="flex flex-col">
-            <h2 class="text-2xl font-bold leading-none">My Location</h2>
-            <h3 class="font-semibold pb-4 pt-1"><%= @my_location.name %></h3>
-            <h4 class="font-semibold"><%= @my_location.weather.current.description.text %></h4>
-          </div>
-          <div class="flex flex-col h-full justify-between items-end">
-            <span class="text-5xl font-light leading-[0.9]"><%= @my_location.weather.current.temperature %>&deg;</span>
-            <div class="flex justify-end gap-5 font-semibold">
-              <h4>L: <%= List.first(@my_location.weather.daily)[:temperature][:low] %>&deg;</h4>
-          <h4>H: <%= List.first(@my_location.weather.daily)[:temperature][:high] %>&deg;</h4>
+        <%= if is_nil(@search_results) do %>
+          <%!-- MY LOCATION (CUSTOM SIZE) --%>
+          <div
+            id="myLocation"
+            phx-click="set_main_weather_to_my_location"
+            class="flex justify-between items-center rounded-xl bg-red-dark py-3.5 px-4 cursor-pointer"
+          >
+            <div class="flex flex-col">
+              <h2 class="text-2xl font-bold leading-none">My Location</h2>
+              <h3 class="font-semibold pb-4 pt-1"><%= @my_location.name %></h3>
+              <h4 class="font-semibold"><%= @my_location.weather.current.description.text %></h4>
+            </div>
+            <div class="flex flex-col h-full justify-between items-end">
+              <span class="text-5xl font-light leading-[0.9]">
+                <%= @my_location.weather.current.temperature %>&deg;
+              </span>
+              <div class="flex justify-end gap-5 font-semibold">
+                <h4>L: <%= List.first(@my_location.weather.daily)[:temperature][:low] %>&deg;</h4>
+                <h4>H: <%= List.first(@my_location.weather.daily)[:temperature][:high] %>&deg;</h4>
+              </div>
             </div>
           </div>
-        </div>
-        <%!-- SAVED PLACES --%>
-        <%!-- # TODO: add "load place" click attribute to card, and event handler to show weather for that place in main --%>
-        <%!-- # TODO: add "delete place" click attribute to button, and event handler to show weather for that place in main --%>
-        <%= @saved_places |> Enum.with_index |> Enum.map(fn({place, index}) -> %>
-          <.place_card id={"placeCard-#{index}"} place={place} myself={@myself} />
-        <% end) %>
+          <%!-- SAVED PLACES --%>
+          <%!-- # TODO: add "load place" click attribute to card, and event handler to show weather for that place in main --%>
+          <%!-- # TODO: add "delete place" click attribute to button, and event handler to show weather for that place in main --%>
+          <%= @saved_places |> Enum.with_index |> Enum.map(fn({place, index}) -> %>
+            <.place_card id={"placeCard-#{index}"} place={place} myself={@myself} />
+          <% end) %>
+        <% end %>
       </div>
     </div>
     """
@@ -95,6 +93,15 @@ defmodule HimmelWeb.PlacesLive do
       )
 
     {:noreply, socket}
+  end
+
+  def handle_event("set_search", %{"name" => name}, socket) do
+    socket = if name == "", do: assign(socket, search_results: nil), else: socket
+    {:noreply, assign(socket, search: name)}
+  end
+
+  def handle_event("clear_search", _, socket) do
+    {:noreply, assign(socket, search: "", search_results: nil)}
   end
 
   # TODO: don't add place if already in saved places
@@ -151,14 +158,29 @@ defmodule HimmelWeb.PlacesLive do
 
   def place_card(assigns) do
     ~H"""
-    <div id={@id} phx-target={@myself} phx-click="set_main_weather" phx-value-id={@place.id} class="flex justify-between items-center rounded-xl bg-red-dark py-3.5 px-4 cursor-pointer">
+    <div
+      id={@id}
+      phx-target={@myself}
+      phx-click="set_main_weather"
+      phx-value-id={@place.id}
+      class="flex justify-between items-center rounded-xl bg-red-dark py-3.5 px-4 cursor-pointer"
+    >
       <div class="flex flex-col">
         <h2 class="text-2xl font-bold leading-none"><%= @place.name %></h2>
         <h3 class="font-semibold pb-4"><%= @place.weather.current.description.text %></h3>
-        <button class="cursor-pointer text-red-light text-left h-6 w-6" phx-target={@myself} phx-click="remove_place" phx-value-id={@place.id}><.icon_trash /></button>
+        <button
+          class="cursor-pointer text-red-light text-left h-6 w-6"
+          phx-target={@myself}
+          phx-click="remove_place"
+          phx-value-id={@place.id}
+        >
+          <.icon_trash />
+        </button>
       </div>
       <div class="flex flex-col h-full justify-between items-end">
-        <span class="text-5xl font-light leading-[0.9]"><%= @place.weather.current.temperature %>&deg;</span>
+        <span class="text-5xl font-light leading-[0.9]">
+          <%= @place.weather.current.temperature %>&deg;
+        </span>
         <div class="flex justify-end gap-5 font-semibold">
           <h4>L: <%= List.first(@place.weather.daily)[:temperature][:low] %>&deg;</h4>
           <h4>H: <%= List.first(@place.weather.daily)[:temperature][:high] %>&deg;</h4>
@@ -168,19 +190,99 @@ defmodule HimmelWeb.PlacesLive do
     """
   end
 
+  def search_bar(assigns) do
+    ~H"""
+    <search>
+      <form phx-submit="search_places" phx-change="set_search" phx-target={@myself}>
+        <div class="inline-flex items-center justify-between w-full h-10 rounded-xl bg-red-dark text-red-light py-2 pl-2">
+          <div class="relative w-full">
+            <input
+              type="text"
+              name="name"
+              value={@search}
+              phx-debounce="300"
+              placeholder="Search for a city or place"
+              autocomplete="off"
+              class="w-full bg-transparent text-white placeholder:text-red-light pl-2 pr-8"
+            />
+            <%= if @search !== "" do %>
+              <span
+                phx-click="clear_search"
+                phx-target={@myself}
+                class="absolute right-0 top-0 h-full w-8 bg-red-dark flex place-content-center cursor-pointer"
+              >
+                <.icon_x_circle />
+              </span>
+            <% end %>
+          </div>
+          <button class="px-4 outline-none">
+            <.icon_loupe />
+          </button>
+        </div>
+      </form>
+    </search>
+    """
+  end
+
   def icon_loupe(assigns) do
     ~H"""
-    <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-auto"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607z"/></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      aria-hidden="true"
+      class="w-6 h-6 mx-auto"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607z"
+      />
+    </svg>
     """
   end
 
   def icon_trash(assigns) do
     ~H"""
-    <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-auto"><path stroke-linecap="round" stroke-linejoin="round" d="M3 6h18M6 6V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2m-2 0v12a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V6h2z"/></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      aria-hidden="true"
+      class="w-6 h-6 mx-auto"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3 6h18M6 6V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2m-2 0v12a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V6h2z"
+      />
+    </svg>
     """
   end
 
-  def get_place_from_search_results(place_id, socket) do
+  def icon_x_circle(assigns) do
+    ~H"""
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
+      class="w-6 h-6 mx-auto"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    """
+  end
+
+  defp get_place_from_search_results(place_id, socket) do
     Enum.find(socket.assigns[:search_results], fn result ->
       result.id == String.to_integer(place_id)
     end)
