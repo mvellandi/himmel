40595f9b17d6abf7062ddcf1fed4cc05d1db5600
Mario Vellandi <mvellandi@hotmail.com>
2023-10-05 22:44:37 +0200

    + add changeset for Place changeset, cast_embed

--- lib/himmel/places/coordinates.ex
+++ lib/himmel/places/coordinates.ex
@@ -1,8 +1,15 @@
 defmodule Himmel.Places.Coordinates do
   use Ecto.Schema
+  import Ecto.Changeset
 
   embedded_schema do
     field :latitude, :float
     field :longitude, :float
+  end
+
+  def changeset(coordinates, attrs) do
+    coordinates
+    |> cast(attrs, [:latitude, :longitude])
+    |> validate_required([:latitude, :longitude])
   end
 end
