9b2cfc86e06b1e8117ec695469d409ecc340e7da
Mario Vellandi <mvellandi@hotmail.com>
2023-10-09 20:01:24 +0200

    + add WeatherData as embedded schema

---
+++ lib/himmel/weather/weather_data.ex
@@ -0,0 +1,16 @@
+defmodule Himmel.Weather.WeatherData do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  embedded_schema do
+    field :current, :map
+    field :daily, {:array, :map}
+    field :hourly, {:array, :map}
+    field :last_updated, :utc_datetime
+  end
+
+  def changeset(weather_data, attrs) do
+    weather_data
+    |> cast(attrs, [:current, :daily, :hourly, :last_updated])
+  end
+end
