f6cd6b8f5cf04172d501d9049d6df23ee051381d
Mario Vellandi <mvellandi@hotmail.com>
2023-10-19 23:23:31 +0200

    ~ mod iex setup if there's no users

--- .iex.exs
+++ .iex.exs
@@ -4,7 +4,7 @@ alias Himmel.Accounts
 alias Himmel.Accounts.User
 alias Himmel.Repo
 
-user = Repo.all(User) |> hd
+user = List.first(Repo.all(User)) || nil
 
 place = %{name: "Hamburg", coordinates: %{latitude: 18.1, longitude: 8.1}}
 places = %{0 => place}
