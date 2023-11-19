56612632d431a1215f7afd7ad4c76d7622a8a90e
Mario Vellandi <mvellandi@hotmail.com>
2023-11-19 19:36:00 +0100

    ~ add typespecs, cleanup, rename vars

--- lib/himmel/weather.ex
+++ lib/himmel/weather.ex
@@ -1,22 +1,25 @@
 defmodule Himmel.Weather do
   alias Himmel.Services
-  alias Himmel.Places.Place
 
-  def get_weather(%Place{location_id: location_id} = place) do
+  @type error_info :: %{type: atom(), stage: atom()}
+
+  @spec get_weather(String.t()) :: {:ok, WeatherInfo.t()} | {:error, error_info() | any()}
+  def get_weather(location_id) do
     case Cachex.get(:weather_cache, location_id) do
       {:ok, nil} ->
-        with {:ok, weather_data} <- Services.Weather.get_weather(place),
-             IO.inspect(weather_data, label: "cache put weather data"),
-             {:ok, true} <- Cachex.put(:weather_cache, location_id, weather_data, ttl: 1_800_000) do
-          {:ok, weather_data}
+        with {:ok, weather = %WeatherInfo{}} <-
+               Services.Weather.get_weather(location_id),
+             {:ok, true} <-
+               Cachex.put(:weather_cache, location_id, weather, ttl: 1_800_000) do
+          {:ok, weather}
         else
           {:error, false} -> {:error, %{type: :cache, stage: :cache_put}}
           {:error, info} -> {:error, info}
         end
 
-      {:ok, weather_data} ->
+      {:ok, weather = %WeatherInfo{}} ->
         {:ok, true} = Cachex.expire(:weather_cache, location_id, 1_800_000)
-        {:ok, weather_data}
+        {:ok, weather}
     end
   end
 end
