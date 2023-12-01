9974892b9ed5ac626bfe55f3196bedfcb17a8d36
Mario Vellandi <mvellandi@hotmail.com>
2023-12-01 15:03:38 +0100

    ~ mod getting ip via x_headers, details via Finch

--- lib/himmel/services/ip.ex
+++ lib/himmel/services/ip.ex
@@ -1,18 +1,38 @@
 defmodule Himmel.Services.IP do
   import Phoenix.LiveView, only: [get_connect_info: 2]
-  alias :ipinfo, as: IPinfo
+  # alias :ipinfo, as: IPinfo
+  alias Himmel.Utils
+  require Logger
 
   @doc "Gets the IP of the requesting client"
   def get_user_ip(socket) do
-    peer_data = get_connect_info(socket, :peer_data)
-    peer_data.address |> :inet.ntoa() |> to_string()
+    get_connect_info(socket, :x_headers)
+    |> Enum.find(fn {key, _} -> key == "x-forwarded-for" end)
+    |> elem(1)
+    |> String.split(",")
+    |> hd()
   end
 
   @doc "Gets the coordinates and city from an IP address"
   def get_ip_details(ip) do
-    with {:ok, %IPinfo{} = handler} <- IPinfo.create(Application.get_env(:ipinfo, :token)),
-         {:ok, details} <- IPinfo.details(handler, ip) do
-      details
+    token = Application.get_env(:ipinfo, :token)
+    url = "https://ipinfo.io/#{ip}?token=#{token}"
+
+    case Utils.web_request(url) do
+      {:ok, %Finch.Response{status: 200, body: body}} ->
+        Jason.decode!(body)
+
+      {:ok, %Finch.Response{status: status}} ->
+        Logger.error("Received status #{status} from IPinfo")
+
+        # TODO: Handle other status codes by returning a default value and notify the liveview that the IPinfo request failed
+        %{city: "Stavanger", loc: "58.97,5.7331"}
+
+      {:error, error} ->
+        Logger.error("Received error #{inspect(error)} from IPinfo")
+
+        # TODO: Handle errors by returning a default value and notify the liveview of a Mint transport error
+        %{city: "Edinburgh", loc: "55.9533,3.1883"}
     end
   end
 end
--- lib/himmel_web/endpoint.ex
+++ lib/himmel_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule HimmelWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [:peer_data, session: @session_options]]
+    websocket: [connect_info: [:peer_data, :x_headers, session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
