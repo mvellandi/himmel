5847f7609ef37d66fd2a3d41ed15521acb0959eb
Mario Vellandi <mvellandi@hotmail.com>
2023-12-02 10:21:11 +0100

    ~ mod generic input form styling

--- lib/himmel_web/components/phx_components.ex
+++ lib/himmel_web/components/phx_components.ex
@@ -369,8 +369,8 @@ defmodule HimmelWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
+          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 p-3",
+          "border-2 border-primary-dark phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
