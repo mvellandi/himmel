989a4aad4ae88d9497e7377886aed4b88c41b486
Mario Vellandi <mvellandi@hotmail.com>
2023-12-04 12:55:33 +0100

    ~ disable login/logout flash banner

--- lib/himmel_web/controllers/user_session_controller.ex
+++ lib/himmel_web/controllers/user_session_controller.ex
@@ -18,12 +18,12 @@ defmodule HimmelWeb.UserSessionController do
     create(conn, params, "Welcome back!")
   end
 
-  defp create(conn, %{"user" => user_params}, info) do
+  defp create(conn, %{"user" => user_params}, _info) do
     %{"email" => email, "password" => password} = user_params
 
     if user = Accounts.get_user_by_email_and_password(email, password) do
       conn
-      |> put_flash(:info, info)
+      # |> put_flash(:info, info)
       |> UserAuth.log_in_user(user, user_params)
     else
       # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
@@ -36,7 +36,7 @@ defmodule HimmelWeb.UserSessionController do
 
   def delete(conn, _params) do
     conn
-    |> put_flash(:info, "Logged out successfully.")
+    # |> put_flash(:info, "Logged out successfully.")
     |> UserAuth.log_out_user()
   end
 end
